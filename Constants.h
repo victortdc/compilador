5981b7cf28506d2ea3a59fea7e22a3d489d86e9e
victortdc <vtdc1998@gmail.com>
2023-04-16 17:41:35 -0300

    Fix

--- Constants.h
+++ Constants.h
@@ -80,10 +80,10 @@ const int ACCEPT = 3;
 const int GO_TO  = 4;
 const int ERROR  = 5;
 
-extern const int PARSER_TABLE[286][122][2];
+extern const int PARSER_TABLE[295][122][2];
 
-extern const int PRODUCTIONS[153][2];
+extern const int PRODUCTIONS[156][2];
 
-extern const char *PARSER_ERROR[286];
+extern const char *PARSER_ERROR[295];
 
 #endif
